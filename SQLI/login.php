7a4b05b65a23a1da8905d1bd795e6fbe2b8fb251
TopiMax2 <paulcuchet1@hotmail.fr>
2023-11-16 21:02:49 +0100

    Finished SQL Injection

--- SQLI/login.php
+++ SQLI/login.php
@@ -1,11 +1,11 @@
 <?php
 // Simulated authentication logic (vulnerable to SQL injection)
-$servername = "your_database_server";
-$username = "your_database_username";
-$password = "your_database_password";
-$dbname = "your_database_name";
+$servername = "127.0.0.1";
+$username = "root";
+$password = "root";
+$dbname = "sqli_data";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, 3306);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -19,7 +19,7 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // Redirect to another page upon successful login
-    header("adminpage.html");
+    include("adminpage.html");
     exit();
 } else {
     echo "Login failed";
